43913f52ccc3e85c706524ec83ebd6b465582449
idocats <57145809+idocats@users.noreply.github.com>
2021-10-12 18:06:36 -0400

    Keep all the bad code

--- public/leafletMap.js
+++ public/leafletMap.js
@@ -2,50 +2,57 @@
 // const require = createRequire(import.meta.url);
 // const mysqlConnection = require("./testconnection");
 
-// const { get } = require("http");
-import React, { useState } from 'react';
 
 let width = 960,
     height = 700,
     centered;
 
-let data_we_want = []
-
-
 // Promise.all first loads these files and will run the function afterwards.
-async function main() {
-
-    // Promise.all([
-    //     d3.json("thailand.json"),
-    //     // d3.csv("coordinates.csv")
-    // ]).then(([provinceData]) => {
-        let coordinates = await dumb_data("SELECT * FROM nasafirmdata order by acq_date")
-
-        // console.log(coordinates)
-        // console.log(data_we_want)
-        console.log(coordinates)
-        createMap({}, coordinates)
-    // })
-}
-main()
-
-async function dumb_data(query) {
-    let json = await fetch('/getdata', {
+Promise.all([
+    d3.json("thailand.json"),
+    // get_data("SELECT * FROM nasafirmdata order by acq_date")
+]).then(([provinceData]) => {
+    // let coordinates = dumb_data("SELECT * FROM nasafirmdata order by acq_date")
+    fetch('/getdata', {
         method:'POST',
-        body: JSON.stringify( { 'query' : query} ),
+        body: JSON.stringify( { 'query' : 'SELECT * FROM nasafirmdata order by acq_date'} ),
         headers : {
             "Content-Type" : "application/json"
         }
     })
         .then(res => res.json())
         .then(json => {
-            return json
+            createMap(provinceData, json)
         })
-    console.log(json.json())
-    return json
+    // let data = get_data('SELECT * FROM nasafirmdata order by acq_date')
+    // console.log(data)
+    // create_map_skelaton()
+})
+
+function create_map_skelaton() {
+    var map = new L.Map("map", {center: [13.7, 100.5], zoom: 5, minZoom: 4, maxZoom: 12})
+    map.addLayer(new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"));
+    // TODO add map.on("zoom")
+
+    // TODO this is scuffed
+    map.on("zoomend", function(){
+        draw_provinces(map);
+    });
+
+    // add geoJSON
+    draw_provinces(map)
+
+    // add data
+    draw_fires(map, "SELECT * FROM nasafirmdata order by acq_date")
 }
 
-function get_data(query) {
+function draw_fires(map, query, provinceData) {
+
+    function projectPoint(x, y) {
+        let point = map.latLngToLayerPoint(new L.LatLng(y, x));
+        this.stream.point(point.x, point.y);
+    }
+
     fetch('/getdata', {
         method:'POST',
         body: JSON.stringify( { 'query' : query} ),
@@ -54,13 +61,208 @@ function get_data(query) {
         }
     })
         .then(res => res.json())
-        .then(json => {
-            return new Promise(resolve => {
-                console.log(json[0])
-                resolve(json)
-            })
+        .then(coordinates => {
+            console.log(coordinates)
+
+            let allGroup = d3.map(coordinates, function(d){return(d.acq_date);}); // This takes the entire column for the date
+            let uniqueDates = allGroup.filter((item, i, ar) => ar.indexOf(item) === i);
+            let dataForDate = coordinates.filter(function(d) {
+                return d["acq_date"] === uniqueDates[0]
+            });
+
+            let svg1 = d3.select(map.getPanes().overlayPane).append("svg").attr("class", "circle-layer"),
+                g1 = svg1.append("g").attr("class", "inner-CL");
+
+            let transform = d3.geoTransform({point: projectPoint}),
+                path = d3.geoPath().projection(transform);
+
+            g1.selectAll("circle")
+                .data(dataForDate)
+                .enter().append("circle");
+
+            g1.attr("id", "inner-CL");
+
+            // Selector. Selects all the circle elements in the inner-CL div
+            let allCircles = document.querySelectorAll(".inner-CL > circle");
+
+            // Sets all the attributes for each circle element
+            for (var i = 0; i < dataForDate.length; i++) {
+                if (coordinates[i].latitude > 5 && dataForDate[i].latitude < 20) { // This only sets attributes if the coordinates are within this latitude
+                    if (dataForDate[i].longitude > 98 && dataForDate[i].longitude < 105) { // and within this longitude
+                        allCircles[i].classList.add(i);
+                        allCircles[i].setAttribute("r", 4);
+                        allCircles[i].setAttribute("stroke-width", 1);
+                        allCircles[i].setAttribute("stroke", "#4F442B");
+                        allCircles[i].setAttribute("fill", "#FFD061");
+                        allCircles[i].setAttribute("cx", map.latLngToLayerPoint(new L.LatLng(dataForDate[i].latitude, dataForDate[i].longitude)).x); // This projects the coordinate from LatLng to the correct pixel on the webpage
+                        allCircles[i].setAttribute("cy", map.latLngToLayerPoint(new L.LatLng(dataForDate[i].latitude, dataForDate[i].longitude)).y);
+
+                        // This function runs when the dot is clicked
+                        allCircles[i].onclick = function () {
+
+                            wildfireID = this.className.animVal
+
+                            console.log(wildfireID)
+                            console.log(coordinates[wildfireID])
+
+                            // Updates the table with the correct information for that specific wildfire
+                            document.querySelector('#acqDate').innerHTML = dataForDate[wildfireID].acq_date
+                            document.querySelector('#acqTime').innerHTML = dataForDate[wildfireID].acq_time
+                            document.querySelector('#latitude').innerHTML = dataForDate[wildfireID].latitude
+                            document.querySelector('#longitude').innerHTML = dataForDate[wildfireID].longitude
+                            document.querySelector('#dOrN').innerHTML = dataForDate[wildfireID].daynight
+                            document.querySelector('#satellite').innerHTML = dataForDate[wildfireID].satellite
+                            document.querySelector('#version').innerHTML = dataForDate[wildfireID].version
+                        };
+
+                        // What happens when the mouse hovers over the dot
+                        allCircles[i].onmouseover = function () {
+                            this.setAttribute('fill', '#F50D00')
+                        }
+
+                        // What happens when the mouse no longer hovers over the dot
+                        allCircles[i].onmouseout = function () {
+                            this.setAttribute('fill', '#FFD061')
+                        }
+                    }
+                }
+            }
+
+            svg1.attr("width", 2000)
+                .attr("height", 2000)
+                .attr("pointer-events", "none"); // This allows us to click through the svg layer so that we can click on the dots
+
+            map.on("zoom", () => reset(map, dataForDate))
         })
+
 }
+
+function reset(map, dataForDate) {
+    console.log("starting reset")
+    let svg1 = document.getElementsByClassName("circle-layer")
+    let g1 = document.getElementsByClassName('inner-CL')
+
+    function projectPoint(x, y) {
+        let point = map.latLngToLayerPoint(new L.LatLng(y, x));
+        this.stream.point(point.x, point.y);
+    }
+
+    let transform = d3.geoTransform({point: projectPoint}),
+        path = d3.geoPath().projection(transform);
+
+    // let bounds = path.bounds(provinceData),
+    //     topLeft = bounds[0],
+    //     bottomRight = bounds[1];
+
+    let allCircles = document.querySelectorAll(".inner-CL > circle");
+
+    // Sets all the attributes for each circle element
+    for (var i = 0; i < dataForDate.length; i++) {
+        allCircles[i].classList.add(i);
+        allCircles[i].setAttribute("r", 4);
+        allCircles[i].setAttribute("stroke-width", 1);
+        allCircles[i].setAttribute("stroke", "#4F442B");
+        allCircles[i].setAttribute("fill", "#FFD061");
+        allCircles[i].setAttribute("cx", map.latLngToLayerPoint(new L.LatLng(dataForDate[i].latitude, dataForDate[i].longitude)).x); // This projects the coordinate from LatLng to the correct pixel on the webpage
+        allCircles[i].setAttribute("cy", map.latLngToLayerPoint(new L.LatLng(dataForDate[i].latitude, dataForDate[i].longitude)).y);
+
+        // This function runs when the dot is clicked
+        allCircles[i].onclick = function () {
+
+            // Updates the table with the correct information for that specific wildfire
+            document.querySelector('#acqDate').innerHTML = dataForDate[wildfireID].acq_date
+            document.querySelector('#acqTime').innerHTML = dataForDate[wildfireID].acq_time
+            document.querySelector('#latitude').innerHTML = dataForDate[wildfireID].latitude
+            document.querySelector('#longitude').innerHTML = dataForDate[wildfireID].longitude
+            document.querySelector('#dOrN').innerHTML = dataForDate[wildfireID].daynight
+            document.querySelector('#satellite').innerHTML = dataForDate[wildfireID].satellite
+            document.querySelector('#version').innerHTML = dataForDate[wildfireID].version
+        };
+
+        // What happens when the mouse hovers over the dot
+        allCircles[i].onmouseover = function () {
+            this.setAttribute('fill', '#F50D00')
+        }
+
+        // What happens when the mouse no longer hovers over the dot
+        allCircles[i].onmouseout = function () {
+            this.setAttribute('fill', '#FFD061')
+        }
+    }
+
+
+    // svg1.style("left", topLeft[0] + "px")
+    //     .style("top", topLeft[1] + "px")
+    //     .attr("width", 2000*map.getZoom())
+    //     .attr("height", 2000*map.getZoom());
+    //
+    // g1.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")")
+    //     .attr("pointer-events", "all");;
+    //
+    // const allCircles = document.querySelectorAll(".inner-CL > circle");
+    // for(i = 0; i < dataForDate.length; i++) {
+    //     allCircles[i].setAttribute("cx", map.latLngToLayerPoint(new L.LatLng(dataForDate[i].latitude, dataForDate[i].longitude)).x);
+    //     allCircles[i].setAttribute("cy", map.latLngToLayerPoint(new L.LatLng(dataForDate[i].latitude, dataForDate[i].longitude)).y);
+    // }
+}
+
+function draw_provinces(map) {
+    if(document.getElementById("province_json_data").innerText == '') {
+        Promise.all([
+            d3.json("thailand.json")])
+            .then(([provinceData]) => {
+                document.getElementById("province_json_data").innerText = JSON.stringify(provinceData)
+                let provinces = new Array;
+                let j;
+                for (j = 0; j < provinceData.features.length; j++) {
+                    provinces.push(provinceData.features[j]);
+                }
+                let geojson = L.geoJSON(provinces, {
+                    style: style//,
+                    // onEachFeature: onEachFeature
+                }).addTo(map);
+
+                // TODO this is scuffed
+                map.on("zoomend", function () {
+                    geojson.removeFrom(map);
+                });
+
+                function style(feature) {
+                    if (map.getZoom() > 9) {
+                        return {
+                            "color": "blue",
+                            "weight": "0",
+                            "fillOpacity": "0"
+                        }
+                    } else {
+                        return {
+                            "color": "blue",
+                            "fillOpacity": "0"
+                        }
+                    }
+                }
+            })
+    }
+    else {
+
+    }
+}
+
+
+//
+// function get_data(query) {
+//     fetch('/getdata', {
+//         method:'POST',
+//         body: JSON.stringify( { 'query' : query} ),
+//         headers : {
+//             "Content-Type" : "application/json"
+//         }
+//     })
+//         .then(res => res.json())
+//         .then(json => {
+//             resolve(json)
+//         })
+// }
 
 /**
  * This function is everything from creating the leaflet map to drawing the geoJSON
@@ -68,32 +270,24 @@ function get_data(query) {
  * @param provinceData the geoJSON file with the province data
  * @param coordinates the csv file with the wildfire data
  */
-function createMap(provinceData) {
+function createMap(provinceData, coordinates) {
 
+    console.log(coordinates)
 
+    let data_that_we_want = get_data_query('SELECT * FROM ? where acq_date="2021-10-11"')
+    console.log(data_that_we_want)
 
     var allGroup = d3.map(coordinates, function(d){return(d.acq_date);}); // This takes the entire column for the date
-
-    // uniqueDates is an array of the unique dates in coordinates. It is used to determine the max value of the slider
-    // As well as indexing through it to filter for different dates from the coordinates.csv data
     let uniqueDates = allGroup.filter((item, i, ar) => ar.indexOf(item) === i);
-
-    // dataForDate returns the rows from the csv that have the desired date the slider is selecting. We start with the first date
+    console.log(uniqueDates)
     var dataForDate = coordinates.filter(function(d) {
         return d["acq_date"] === uniqueDates[0]
     });
 
-
-    // Get the slider and its output div from the HTML
     var slider = document.getElementById("mySlider"),
         output = document.getElementById("sliderValue");
-
     slider.setAttribute("max", (uniqueDates.length - 1)); // set the number of slider points to be equal to the number of unique dates from the data
-
     output.innerHTML = uniqueDates[slider.value]; // Output the slider value to equal the date so the user knows what data is currently being shown
-
-    // This updates the date that is displayed to the user every time the user changes the slider value.
-    // It also runs changeDate() which updates the wildfire data points on the map.
     slider.oninput = function () {
         changeDate();
         output.innerHTML = uniqueDates[slider.value];
@@ -101,7 +295,15 @@ function createMap(provinceData) {
 
     // Create/attach the leaflet map to our map div.
     var map = new L.Map("map", {center: [13.7, 100.5], zoom: 5, minZoom: 4, maxZoom: 12})
-        .addLayer(new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"));
+    map.addLayer(new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"));
+
+    map.on("zoom", reset); // When the user zooms in or out, this function should run
+    map.on("zoomend", function(){
+        geojson.removeFrom(map);
+        addProvinces();
+    });
+
+
 
 
     // Add provinces from geoJSON file.
@@ -113,8 +315,6 @@ function createMap(provinceData) {
     }
 
     var geojson;
-
-    // Adds the provinces to the leaflet map
     function addProvinces() {
         geojson = L.geoJSON(provinces, {
             style: style,
@@ -150,6 +350,7 @@ function createMap(provinceData) {
     // What happens when you clean on the geoJSON feature.
     // e is the mouse event. e.target is the province object
     function zoomToFeature(e) {
+        console.log("Starting to fetch new data")
         map.flyToBounds(e.target.getBounds());
         console.log("The name of the province is" + e.target.feature.properties.name);
 
@@ -176,7 +377,6 @@ function createMap(provinceData) {
             console.log(allCoord);
         }
     }
-    var allCoord = new Array; // An array for all the coordinates inside the province
 
     // Finds the overlayPane div and places an svg layer inside that has a class name of "circle-layer"
     // There is also a g div in the svg with a name of "inner-CL"
@@ -185,12 +385,9 @@ function createMap(provinceData) {
     var svg1 = d3.select(map.getPanes().overlayPane).append("svg").attr("class", "circle-layer"),
         g1 = svg1.append("g").attr("class", "inner-CL");
 
-    // This takes the path and fits the projection on it using the function below
     var transform = d3.geoTransform({point: projectPoint}),
         path = d3.geoPath().projection(transform);
 
-
-    // Creates all the circle elements in the circle-layer div based on the date selected from dataForDate
     g1.selectAll("circle")
         .data(dataForDate)
         .enter().append("circle");
@@ -250,10 +447,7 @@ function createMap(provinceData) {
         .attr("pointer-events", "none"); // This allows us to click through the svg layer so that we can click on the dots
 
 
-    /**
-     * Handles the slider moving position and updates the map so that the data displayed corresponds to the correct date
-     * @param e is a mouse click event
-     */
+
     function changeDate(e) {
         console.log("slider value: " + document.getElementById("mySlider").value);
 
@@ -310,16 +504,6 @@ function createMap(provinceData) {
         }
     }
 
-    map.on("zoom", reset); // When the user zooms in or out, this function should run
-
-    // After the map finished zooming run this function. All it does is remove the geoJSON objects and adds them back again
-    // This is needed because we have different styling depending on the zoom level. When you zoom in far enough,
-    // the border for the provinces dissapear as they are not as detailed as the ones included in the leaflet map
-    map.on("zoomend", function(){
-        geojson.removeFrom(map);
-        addProvinces();
-    });
-
     reset();
 
     /**
@@ -327,8 +511,7 @@ function createMap(provinceData) {
      * needs to recalculate how big it is. The wildfire data also needs to recalculate where to draw itself.
      */
     function reset() {
-        console.log("Starting reset");
-        var bounds = path.bounds(provinceData),
+        let bounds = path.bounds(provinceData),
             topLeft = bounds[0],
             bottomRight = bounds[1];
 
@@ -353,4 +536,9 @@ function createMap(provinceData) {
         this.stream.point(point.x, point.y);
     }
 
+    function get_data_query(query) {
+        return alasql(query,[coordinates]);
+    }
+
 }
+
