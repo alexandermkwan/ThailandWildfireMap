966806bc43a2ffdf6dd8b0edef8b4d73bc9ca8a5
AKWAN <alexander.mkwan@gmail.com>
2021-10-21 18:27:25 -0400

    affected Regions bar chart code inserted

--- public/leafletMap.js
+++ public/leafletMap.js
@@ -593,10 +593,6 @@ function createMap(provinceData, coordinates, fire_data) {
         return averages
     }
 
-
-
-
-    
     runningAverageCalcForProvince("Mae Hong Son");
 
     let was_draw_line_created = false
@@ -620,7 +616,146 @@ function createMap(provinceData, coordinates, fire_data) {
     }
 
     document.getElementById("affectedRegions").onclick = () => {
-        console.log("Showing most affected regions");
+        Promise.all([
+            d3.json("thailand.json"),
+            d3.csv("coordinates.csv")
+        ]).then( ([provinceData ,coordinates]) => {
+            var provincesWithFireCount = []
+            var provinces = new Array;
+            var i;
+            for (i = 0; i < provinceData.features.length; i++) {
+                provinces.push(provinceData.features[i]);
+            }
+
+            let j;
+            for (j = 0; j < provinceData.features.length; j++) {
+                var count = countFiresInsideProvince(provinces[j]);
+                provincesWithFireCount.push({"name": provinces[j].properties.name, "count": count});
+            }
+
+            // sort by value
+            provincesWithFireCount.sort(function (a, b) {
+                return a.count - b.count;
+            });
+
+            // This function finds all the dots within the feature returns the count of dots
+            function countFiresInsideProvince(feature) {
+
+                var fireCount = 0;
+                var dataForDate = coordinates.filter(function (d) {
+                    return d["acq_date"] === "2021-01-28"
+                });
+                allCoord = new Array;
+                var i;
+                for (i = 0; i < dataForDate.length; i++) {
+                    if (d3.geoContains(feature, [dataForDate[i].longitude, dataForDate[i].latitude])) {
+                        fireCount = fireCount + 1;
+                    }
+                }
+                return fireCount;
+            }
+
+            var yname = []
+            var xcount = []
+
+            for (let k = 0; k < provincesWithFireCount.length; k++) {
+                yname.push(provincesWithFireCount[k].name)
+            }
+
+            for (let yc = 0; yc < provincesWithFireCount.length; yc++) {
+                xcount.push(provincesWithFireCount[yc].count)
+            }
+
+            //remove zero from xcount
+            function removeElementsWithValue(arr, val) {
+                var i = arr.length;
+                while (i--) {
+                    if (arr[i] === val) {
+                        arr.splice(i, 1);
+                    }
+                }
+                return arr;
+            }
+
+            removeElementsWithValue(xcount, 0)
+            removeElementsWithValue(xcount, 1)
+            removeElementsWithValue(xcount, 2)
+            removeElementsWithValue(xcount, 3)
+
+            var xl = 77 - xcount.length;
+            while(xl--){
+                yname.shift()
+            }
+
+            // set the dimensions and margins of the graph
+            const margin = {top: 100, right: 0, bottom: 0, left: 0},
+                width = 460 - margin.left - margin.right,
+                height = 460 - margin.top - margin.bottom,
+                innerRadius = 90,
+                outerRadius = Math.min(width, height) / 2;   // the outerRadius goes from the middle of the SVG area to the border
+
+            // append the svg object
+            const svg = d3.select("#my_dataviz")
+                .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", `translate(${width/2+margin.left}, ${height/2+margin.top})`);
+
+            for (let k = 0; k < xcount.length; k++) {
+                xcount[k] = xcount[k] * 300
+            }
+
+            console.log(xcount)
+            console.log(yname)
+            console.log(provincesWithFireCount)
+
+            const obj = xcount
+            let yInd = -1
+            const data = Object.entries(obj).map(element => {
+                yInd = yInd + 1
+                return {
+                    Country: yname[yInd],
+                    Value: element[1]
+                }
+            });
+
+            // Scales
+            const x = d3.scaleBand()
+                .range([0, 2 * Math.PI])    // X axis goes from 0 to 2pi = all around the circle. If I stop at 1Pi, it will be around a half circle
+                .align(0)                  // This does nothing
+                .domain(data.map(d => d.Country)); // The domain of the X axis is the list of states.
+            const y = d3.scaleRadial()
+                .range([innerRadius, outerRadius])   // Domain will be define later.
+                .domain([0, 14000]); // Domain of Y is from 0 to the max seen in the data
+
+            // Add the bars
+            svg.append("g")
+                .selectAll("path")
+                .data(data)
+                .join("path")
+                .attr("fill", "#69b3a2")
+                .attr("d", d3.arc()     // imagine your doing a part of a donut plot
+                    .innerRadius(innerRadius)
+                    .outerRadius(d => y(d['Value']))
+                    .startAngle(d => x(d.Country))
+                    .endAngle(d => x(d.Country) + x.bandwidth())
+                    .padAngle(0.01)
+                    .padRadius(innerRadius))
+
+            // Add the labels
+            svg.append("g")
+                .selectAll("g")
+                .data(data)
+                .join("g")
+                .attr("text-anchor", function(d) { return (x(d.Country) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
+                .attr("transform", function(d) { return "rotate(" + ((x(d.Country) + x.bandwidth() / 2) * 180 / Math.PI - 90) + ")"+"translate(" + (y(d['Value'])+10) + ",0)"; })
+                .append("text")
+                .text(function(d){return(d.Country)})
+                .attr("transform", function(d) { return (x(d.Country) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
+                .style("font-size", "11px")
+                .attr("alignment-baseline", "middle")
+        })
     }
 
     document.getElementById("showToday").onclick = () => {
