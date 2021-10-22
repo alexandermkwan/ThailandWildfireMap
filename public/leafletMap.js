fadedd1bb2dfa4a126d81a1a517b8860995d4d2b
idocats <57145809+idocats@users.noreply.github.com>
2021-10-22 15:12:31 -0400

    final fixes

--- public/leafletMap.js
+++ public/leafletMap.js
@@ -686,70 +686,76 @@ function createMap(provinceData, coordinates, fire_data) {
                 yname.shift()
             }
 
-            // set the dimensions and margins of the graph
-            const margin = {top: 100, right: 0, bottom: 0, left: 0},
-                width = 460 - margin.left - margin.right,
-                height = 460 - margin.top - margin.bottom,
-                innerRadius = 90,
-                outerRadius = Math.min(width, height) / 2;   // the outerRadius goes from the middle of the SVG area to the border
+            if(xcount.length > 1) {
+                // set the dimensions and margins of the graph
+                const margin = {top: 100, right: 0, bottom: 0, left: 0},
+                    width = 460 - margin.left - margin.right,
+                    height = 460 - margin.top - margin.bottom,
+                    innerRadius = 90,
+                    outerRadius = Math.min(width, height) / 2;   // the outerRadius goes from the middle of the SVG area to the border
 
-            // append the svg object
-            const svg = d3.select("#my_dataviz")
-                .append("svg")
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform", `translate(${width/2+margin.left}, ${(height/2+margin.top)-50})`);
+                // append the svg object
+                const svg = d3.select("#my_dataviz")
+                    .append("svg")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom)
+                    .append("g")
+                    .attr("transform", `translate(${width/2+margin.left}, ${(height/2+margin.top)-50})`);
 
-            for (let k = 0; k < xcount.length; k++) {
-                xcount[k] = xcount[k] * 300
-            }
-
-            const obj = xcount
-            let yInd = -1
-            const data = Object.entries(obj).map(element => {
-                yInd = yInd + 1
-                return {
-                    Country: yname[yInd],
-                    Value: element[1]
+                for (let k = 0; k < xcount.length; k++) {
+                    xcount[k] = xcount[k] * 300
                 }
-            });
 
-            // Scales
-            const x = d3.scaleBand()
-                .range([0, 2 * Math.PI])    // X axis goes from 0 to 2pi = all around the circle. If I stop at 1Pi, it will be around a half circle
-                .align(0)                  // This does nothing
-                .domain(data.map(d => d.Country)); // The domain of the X axis is the list of states.
-            const y = d3.scaleRadial()
-                .range([innerRadius, outerRadius])   // Domain will be define later.
-                .domain([0, 14000]); // Domain of Y is from 0 to the max seen in the data
+                const obj = xcount
+                let yInd = -1
+                const data = Object.entries(obj).map(element => {
+                    yInd = yInd + 1
+                    return {
+                        Country: yname[yInd],
+                        Value: element[1]
+                    }
+                });
 
-            // Add the bars
-            svg.append("g")
-                .selectAll("path")
-                .data(data)
-                .join("path")
-                .attr("fill", "#69b3a2")
-                .attr("d", d3.arc()     // imagine your doing a part of a donut plot
-                    .innerRadius(innerRadius)
-                    .outerRadius(d => y(d['Value']))
-                    .startAngle(d => x(d.Country))
-                    .endAngle(d => x(d.Country) + x.bandwidth())
-                    .padAngle(0.01)
-                    .padRadius(innerRadius))
+                // Scales
+                const x = d3.scaleBand()
+                    .range([0, 2 * Math.PI])    // X axis goes from 0 to 2pi = all around the circle. If I stop at 1Pi, it will be around a half circle
+                    .align(0)                  // This does nothing
+                    .domain(data.map(d => d.Country)); // The domain of the X axis is the list of states.
+                const y = d3.scaleRadial()
+                    .range([innerRadius, outerRadius])   // Domain will be define later.
+                    .domain([0, 14000]); // Domain of Y is from 0 to the max seen in the data
 
-            // Add the labels
-            svg.append("g")
-                .selectAll("g")
-                .data(data)
-                .join("g")
-                .attr("text-anchor", function(d) { return (x(d.Country) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
-                .attr("transform", function(d) { return "rotate(" + ((x(d.Country) + x.bandwidth() / 2) * 180 / Math.PI - 90) + ")"+"translate(" + (y(d['Value'])+10) + ",0)"; })
-                .append("text")
-                .text(function(d){return(d.Country)})
-                .attr("transform", function(d) { return (x(d.Country) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
-                .style("font-size", "11px")
-                .attr("alignment-baseline", "middle")
+                // Add the bars
+                svg.append("g")
+                    .selectAll("path")
+                    .data(data)
+                    .join("path")
+                    .attr("fill", "#69b3a2")
+                    .attr("d", d3.arc()     // imagine your doing a part of a donut plot
+                        .innerRadius(innerRadius)
+                        .outerRadius(d => y(d['Value']))
+                        .startAngle(d => x(d.Country))
+                        .endAngle(d => x(d.Country) + x.bandwidth())
+                        .padAngle(0.01)
+                        .padRadius(innerRadius))
+
+                // Add the labels
+                svg.append("g")
+                    .selectAll("g")
+                    .data(data)
+                    .join("g")
+                    .attr("text-anchor", function(d) { return (x(d.Country) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
+                    .attr("transform", function(d) { return "rotate(" + ((x(d.Country) + x.bandwidth() / 2) * 180 / Math.PI - 90) + ")"+"translate(" + (y(d['Value'])+10) + ",0)"; })
+                    .append("text")
+                    .text(function(d){return(d.Country)})
+                    .attr("transform", function(d) { return (x(d.Country) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
+                    .style("font-size", "11px")
+                    .attr("alignment-baseline", "middle")
+            }
+            if(xcount.length < 2) {
+                document.getElementById("my_dataviz_container").style.display = "none";
+                alert("Not enough fires to show graph.")
+            }
 
     }
 
